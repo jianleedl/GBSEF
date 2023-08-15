ccf40abcbaa9b31488758e9bc6dcbb6b60eb412b
Aubreyjps <1935874411@qq.com>
2023-08-15 19:00:45 +0800

    update all

---
+++ Attacker/__init__.py
@@ -0,0 +1,7 @@
+from .get_NE_list import NE_list
+from .paraphrase import textfool_perturb_text
+from .paraphrase import adversarial_paraphrase
+from .paraphrase import _generate_synonym_candidates
+from .paraphrase import nlp
+from .paraphrase import GAAdversary
+from .typos import typos
